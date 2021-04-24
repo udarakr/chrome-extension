ed2d9e51986def7c6ed35b345a587bf2e79be478
Udara Rathnayake <udarakr@gmail.com>
2021-04-24 23:16:35 +0530

    env specific config

--- sdk-poc/config/webpack.config.js
+++ sdk-poc/config/webpack.config.js
@@ -4,35 +4,37 @@ const { merge } = require("webpack-merge");
 const replace = require("replace-in-file-webpack-plugin");
 const common = require("./webpack.common.js");
 const PATHS = require("./paths");
+const config = require("./environment.json");
 
-module.exports = merge(common, {
-  entry: {
-    index: PATHS.src + "/scripts/index.js",
-    background: PATHS.src + "/scripts/background.js",
-    styles: PATHS.src + "/styles/main.css"
-  },
-  plugins: [
-    new replace([
-      {
-        dir: "build",
-        files: ["manifest.json"],
-        rules: [
-          {
-            search: "<APP_PERMISSION>",
-            replace: "https://localhost:3101/*"
-          },
-        ],
-      },
-      {
-        dir: "build",
-        files: ["index.js"],
-        rules: [
-          {
-            search: /<APP_HOST>/ig, 
-            replace: "https://localhost:3101"
-          },
-        ],
-      },
-    ]),
-  ],
-});
+module.exports = (env) =>
+  merge(common, {
+    entry: {
+      index: PATHS.src + "/scripts/index.js",
+      background: PATHS.src + "/scripts/background.js",
+      styles: PATHS.src + "/styles/main.css",
+    },
+    plugins: [
+      new replace([
+        {
+          dir: "build",
+          files: ["manifest.json"],
+          rules: [
+            {
+              search: "<APP_PERMISSION>",
+              replace: config[env] + "/*",
+            },
+          ],
+        },
+        {
+          dir: "build",
+          files: ["index.js"],
+          rules: [
+            {
+              search: /<APP_HOST>/gi,
+              replace: config[env],
+            },
+          ],
+        },
+      ]),
+    ],
+  });
